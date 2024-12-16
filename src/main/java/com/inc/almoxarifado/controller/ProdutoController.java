70e0e51289627d60b57385dd60b2cd6753ef7ccf
Arthur Azevedo <arthurfranciscoazevedo@gmail.com>
2024-12-15 23:44:37 -0300

    Adicionando o getAll na api

--- src/main/java/com/inc/almoxarifado/controller/ProdutoController.java
+++ src/main/java/com/inc/almoxarifado/controller/ProdutoController.java
@@ -4,20 +4,26 @@ import com.inc.almoxarifado.model.Product;
 import com.inc.almoxarifado.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/almo-sys/products")
+@RequestMapping("/almo-sys")
 public class ProdutoController {
 
     @Autowired
     private ProductService productService;
 
-    @PostMapping
+    @PostMapping("products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productService.createProduct(product));
     }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+
 }
--- src/main/java/com/inc/almoxarifado/service/ProductService.java
+++ src/main/java/com/inc/almoxarifado/service/ProductService.java
@@ -5,6 +5,8 @@ import com.inc.almoxarifado.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -13,5 +15,9 @@ public class ProductService {
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
